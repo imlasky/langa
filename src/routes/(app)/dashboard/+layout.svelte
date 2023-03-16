7c732ba97a70debd60ff747c4b5951820d6bca1e
Ian Lasky <ianmlasky@gmail.com>
2023-03-16 16:26:51 -0400

    made layouts separate for dashboard and other content

--- src/routes/(app)/dashboard/+layout.svelte
+++ src/routes/(app)/dashboard/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
+    export let data;
 
 </script>
 <div class="flex flex-col sm:flex-row w-full">
     <div class="flex flex-row sm:flex-col justify-center sm:justify-start w-full sm:w-1/6 sm:h-screen">
+        <h1 class="normal-case text-3xl" >Langa</h1>
         <ul class="menu menu-horizontal sm:menu-vertical w-full justify-center menu-compact">
             <li><a href="/dashboard">Overview</a></li>
             <li><a href="/dashboard/study">Study</a></li>
